2680576570ca5dfb05279354d6d9443fe8598aef
Coleman McFarland <coleman.mcfarland@gmail.com>
2015-04-26 10:40:24 -0400

    No longer referencing external package, these functions are already in our namespace

--- lexer/LexerFactory.go
+++ lexer/LexerFactory.go
@@ -1,7 +1,6 @@
 package lexer
 
 import (
-	"github.com/anxiousmodernman/dirz/lexer"
 	"github.com/anxiousmodernman/dirz/token"
 )
 
@@ -10,12 +9,12 @@ Start a new lexer with a given input string. This returns the
 instance of the lexer and a channel of tokens. Reading this stream
 is the way to parse a given input and perform processing.
 */
-func BeginLexing(name, input string) *lexer.Lexer {
-	l := &lexer.Lexer{
+func BeginLexing(name string, input string) *Lexer {
+	l := &Lexer{
 		Name:   name,
 		Input:  input,
-		State:  lexer.LexBegin,
-		Tokens: make(chan lexertoken.Token, 3),
+		State:  LexBegin,
+		Tokens: make(chan token.Token, 3),
 	}
 
 	return l
